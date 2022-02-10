7bd50f0169c2099dee1169e618114c165558afe3
Konstantina Souvatzidaki <kwn.souv@gmail.com>
2022-02-10 13:10:25 +0200

    fix pretty printing

--- Computational Math Labs/#1 numpy, sympy/sympy_examples.py
+++ Computational Math Labs/#1 numpy, sympy/sympy_examples.py
@@ -5,28 +5,27 @@ from graphviz import Source
 
 class sympy_examples():
 
-    def __init__(self):
-        sp.init_printing(use_unicode = True)
-
     # ================ Pretty Printing Demo ================
     def demo(self):
         x = sp.symbols( "x" )
-        print(sp.Integral( sp.sin(x) ))
+        sp.pprint(sp.Integral( sp.sin(x) ))
+        print('\n')
 
     
     # ================ Symbolic Computations ================
     def sqrt(self):
-        print(math.sqrt(5))
-        print(sp.sqrt(5),'\n') 
+        sp.pprint(math.sqrt(5))
+        sp.pprint(sp.sqrt(5)) 
+        print('\n')
 
     def subs(self):
         x, y = sp.symbols( "x y" )
         expr = x**2 + 10*x + y**10 + 5*y
-        print(expr) # x**2 + 10*x + y**10 + 5*y
+        sp.pprint(expr) # x**2 + 10*x + y**10 + 5*y
         #substitute all instances of a variable or expression in a mathematical expression with some other variable or expression or value
-        print(expr.subs(x, 5)) # y**10 + 5*y + 75
-        print(expr.subs( x, 5).subs( y, 1 )) # 81
-        print(expr.subs( [ (x,5), (y,1) ]),'\n') # 81
+        sp.pprint(expr.subs(x, 5)) # y**10 + 5*y + 75
+        sp.pprint(expr.subs( x, 5).subs( y, 1 )) # 81
+        sp.pprint(expr.subs( [ (x,5), (y,1) ])) # 81
         Source( sp.dotprint( expr )) # renders a tree visualization of the expression:
         # add |-- pow |-- x
         #     |       |-- 2
@@ -36,6 +35,7 @@ class sympy_examples():
         #     |       |-- y
         #     |-- mul |-- 10
         #             |-- x
+        print('\n')
    
 
     # ================ Factor, Simplify, Expand ================
@@ -43,20 +43,23 @@ class sympy_examples():
         print("factor expressions")
         x = sp.symbols("x")
         expr1 = x**2 + 2*x + 1
-        print(expr1) # x**2 + 2*x + 1
-        print(expr1.factor()) # (x + 1)**2
+        sp.pprint(expr1) # x**2 + 2*x + 1
+        sp.pprint(expr1.factor()) # (x + 1)**2
+        print('\n')
 
     def simplify(self):
         print("simplify expressions")
         x = sp.symbols("x")
         expr2 = sp.sin(x)**2 + sp.cos(x)**2
-        print(expr2.simplify(),'\n') # 1
+        sp.pprint(expr2.simplify()) # 1
+        print('\n')
 
     def expand(self):
         print("expand expressions")
         x,y = sp.symbols("x y")
         expr3 = ( x + y )**3
-        print(expr3.expand(), '\n') # x**3 + 3*x**2*y + 3*x*y**2 + y**3 
+        sp.pprint(expr3.expand()) # x**3 + 3*x**2*y + 3*x*y**2 + y**3 
+        print('\n')
 
     
     # ================ Exercise 1 ================
@@ -65,7 +68,8 @@ class sympy_examples():
         print(" factoring a bigger expression")
         x = sp.symbols("x")
         expr = ( x ** 3 - 3 * x**2 + 3 * x - 1 ) / (  x**2 + 2*x + 1 ) 
-        print(expr.factor(),'\n') # (x - 1)**3/(x + 1)**2
+        sp.pprint(expr.factor()) # (x - 1)**3/(x + 1)**2
+        print('\n')
 
 
     # ================ Call everything ================
@@ -80,5 +84,6 @@ class sympy_examples():
 # main
 if __name__ == "__main__":
     print("Sympy examples")
+    sp.init_printing(use_unicode = True)
     x = sympy_examples()
     x.call_everything()
